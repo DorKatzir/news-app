8866aa8809aac5a596d8a4fb556f5a4a0e8f64aa
Dor Katzir <katzirdor@gmail.com>
2024-08-10 14:19:37 +0300

    NewsModal Structure

--- src/Components/NewsModal.jsx
+++ src/Components/NewsModal.jsx
@@ -1,9 +1,37 @@
-
+import demoImg from '../assets/images/demo.jpg'
 
 export default function NewsModal() {
     return (
-        <div>
-            News Modal
+        <div className="modal-overlay">
+            <div className="modal-content">
+
+                <span className="close-button">
+                    <i className="fa-solid fa-xmark"></i>
+                </span>
+
+                <img src={ demoImg } alt="Modal Image" className="modal-image" />
+
+                <h2 className="modal-title">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, mollitia.
+                </h2>
+
+                <p className="modal-source">
+                    Source: The Guardian
+                </p>
+
+                <p className="modal-date">
+                    Aug 10, 2024, 03:15 PM
+                </p>
+
+                <p className="modal-content-text">
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minus modi dolorem quis id? Consequatur aliquid aspernatur provident explicabo voluptatibus autem accusamus incidunt suscipit nostrum dicta facilis quis est iusto accusantium sint, aut dolor reiciendis veritatis!
+                </p>
+
+                <a href="" className="read-more-link">
+                    Read More
+                </a>
+
+            </div>
         </div>
     )
 }
